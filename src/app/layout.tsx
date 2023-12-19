1dd3c8bc91e05621468fb9e476dc764bbff09257
Cleberson Jr <cleberson.dev@gmail.com>
2023-12-18 22:46:01 -0300

    Change font to Inria Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inria_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inriaSans = Inria_Sans({
+  weight: ["300", "400", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "cleberson.dev",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inriaSans.className}>{children}</body>
     </html>
   );
 }
