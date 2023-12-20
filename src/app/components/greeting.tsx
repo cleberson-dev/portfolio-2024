a1fc54080cc4781d40030caef05ca81035551bfa
Cleberson Jr <cleberson.dev@gmail.com>
2023-12-20 00:28:04 -0300

    Add typewriting effect to greetings

--- src/app/components/greeting.tsx
+++ src/app/components/greeting.tsx
@@ -8,17 +8,31 @@ type Props = {
 export default function Greeting({ greetings }: Props) {
   const [currentGreetingIdx, setCurrentGreetingIdx] = useState(0);
   const currentGreeting = useMemo(
-    () => greetings[currentGreetingIdx % greetings.length],
+    () => greetings[currentGreetingIdx % greetings.length] + "!",
     [currentGreetingIdx, greetings]
   );
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentGreetingIdx((prev) => prev + 1);
-    }, 2000);
+    }, 3000);
 
     return () => clearInterval(interval);
   }, []);
 
-  return <span className="text-2xl">{currentGreeting}! I&apos;m</span>;
+  return (
+    <span className="text-xl">
+      <span className="typewriter-container">
+        <span
+          className="typewriter"
+          style={{
+            animationTimingFunction: `steps(${currentGreeting.length})`,
+          }}
+        >
+          {currentGreeting}
+        </span>
+      </span>
+      <span className="block text-xs">I&apos;m</span>
+    </span>
+  );
 }
