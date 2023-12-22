e854d1f49fd20061b47577e94e40e69b8afbe470
Cleberson Jr <cleberson.dev@gmail.com>
2023-12-22 13:12:00 -0300

    Add Google Tag Manager (TRYING)

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import ThemeProvider from "@/app/theme-provider";
 import { Analytics } from "@vercel/analytics/react";
 import GoogleAnalytics from "../components/google-analytics";
+import Script from "next/script";
+import Head from "next/head";
 
 const inriaSans = Inria_Sans({
   weight: ["300", "400", "700"],
@@ -29,12 +31,42 @@ export default function RootLayout({
 }) {
   return (
     <html lang={params.lang}>
+      <Head>
+        {process.env.NEXT_PUBLIC_GTM_ANALYTICS && (
+          <Script
+            id="gtm-head-script"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ANALYTICS}');
+            `,
+            }}
+          ></Script>
+        )}
+        {/* <!-- End Google Tag Manager --> */}
+      </Head>
       <body
         className={cls([
           inriaSans.className,
           "min-h-[100svh] flex flex-col items-center justify-center",
         ])}
       >
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        {process.env.NEXT_PUBLIC_GTM_ANALYTICS && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ANALYTICS}`}
+              height="0"
+              width="0"
+              className="invisible hidden"
+            ></iframe>
+          </noscript>
+        )}
+        {/* <!-- End Google Tag Manager (noscript) --> */}
+
         <ThemeProvider>{children}</ThemeProvider>
         <Analytics />
 
